5a7f3cba131dd69eb8881a3fd6c6fdf4077367b5
javascript01fan <ejrojas@icloud.com>
2019-10-01 12:45:07 -0400

    object dayClass

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "components/DayListItem.scss";
+const className = require('classnames');
+
+export default function DayListItem(props) {
+    const dayClass = className('day-list__item', {
+        'day-list__item--selected': props.selected,
+        'day-list__item--full': props.spots === 0
+    })
+    const formatSpots = function (spotsNumber) {
+        if (spotsNumber === 0) {
+            return 'no spots remaining'
+        } else if (spotsNumber === 1) {
+            return `${spotsNumber} spot remaining`
+        } else {
+            return `${spotsNumber} spots remaining`
+        }
+    }
+  return (
+    <li className={dayClass} onClick={()=>props.setDate(props.name)} >
+      <h2 className="text--regular">{props.name} </h2> 
+      <h3 className="text--light">{props.spots} </h3>
+    </li>
+  );
+}
