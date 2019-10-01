0595efa720a130f42ece5ca6fcf9472e06399d0c
javascript01fan <ejrojas@icloud.com>
2019-10-01 15:13:57 -0400

    changed in interviewelist

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,50 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
-import DayList from './DayList'
-
+import DayList from "./DayList";
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [day, setDay] = useState("Monday");
 
   const days = [
     {
       id: 1,
       name: "Monday",
-      spots: 3,
+      spots: 3
     },
     {
       id: 2,
       name: "Tuesday",
-      spots: 5,
+      spots: 5
     },
     {
       id: 3,
       name: "Wednesday",
-      spots: 0,
-    },
+      spots: 0
+    }
   ];
-  
+
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} day={day} setDay={ setDay } />  
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      
       </section>
     </main>
   );
