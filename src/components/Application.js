f1cbfa5b105e2d14b1892b666cceb80cfb40e7d3
javascript01fan <ejrojas@icloud.com>
2019-10-03 13:49:01 -0400

    visual mode hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,75 +1,70 @@
 import React, { useState, useEffect } from "react";
+import { getAppointmentsForDay } from "helpers/selectors";
+
+import { getInterview } from "helpers/selectors";
+
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const axios = require('axios');
+const axios = require("axios");
 
-// data
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "4pm"
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Jon Macklain",
-      interviewer: { 
-        id: 3, 
-        name: "Robert Bundabi", 
-        avatar: "https://i.imgur.com/T2WwVfS.png" }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Tony",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-];
+
+
+
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers:{}
+  });
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  
+  
 
   useEffect(() => {
-    axios.get(`/api/days`)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    setDays(response.data)
-     
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-  },[]);
+    Promise.all([
+      Promise.resolve(
+      axios.get(`/api/days`)
+    
+      ),
+      Promise.resolve(
+        axios.get(`/api/appointments`)
+      ),
+      Promise.resolve(axios.get(`/api/interviewers`))
+    ])
+      .then(all => {
+        //console.log(all[0].data); // first
+       // console.log(all[1].data); // second
+       // console.log(all[2].data); // third
+        setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+       // const [first, second, third] = all;
+
+        //console.log(first, second, third);
+      })
+
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -80,7 +75,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -89,7 +84,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(a => <Appointment key={a.id} {...a} />)}
+        {schedule}
+        {appointments.map(a => (
+          <Appointment key={a.id} {...a} />
+        ))}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,4 +8,15 @@ export function getAppointmentsForDay(state, day) {
     }
     return result;
  
+}
+  
+export function  getInterview(state, interview) {
+    if (!interview) {
+        return null;
+    } else {
+        const student = interview.student;
+        const interviewer = state.interviewers[interview.interviewer];
+        const interviewObj = { student, interviewer };
+        return interviewObj;
   }
+}
